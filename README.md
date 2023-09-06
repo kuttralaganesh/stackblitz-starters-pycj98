4ef056eb9f0a19d896af3d1ac180406b2fbfd3b4
kuttralaganesh <44425346+kuttralaganesh@users.noreply.github.com>
2023-09-06 17:23:10 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-starters-pycj98
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-pycj98)
---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import './style.css';
+
+const arr = [
+  { id: 1, name: 'test1', age: 22 },
+  { id: 2, name: 'test2', age: 23 },
+  { id: 3, name: 'test3', age: 24 },
+  { id: 4, name: 'test4', age: 25 },
+  { id: 5, name: 'test5', age: 26 },
+];
+
+export default function App() {
+  const [state, seState] = useState(arr);
+  const [age, setAge] = useState('');
+  const [select, setselect] = useState(null);
+  const [error, setError] = useState(null);
+
+  const handleChange = (e) => {
+    // console.log(e.target.value);
+    setselect(e.target.value);
+  };
+  const formSubmit = (event) => {
+    event.preventDefault();
+
+    const age = arr.filter((item) => {
+      try{
+        const age = select === item.name ? setAge(item.age) : throw new Error();
+      }
+      
+    });
+  };
+
+  console.log(select);
+
+  return (
+    <div>
+      <form onSubmit={(e) => formSubmit(e)}>
+        <input type="text" onChange={(e) => handleChange(e)} />
+        <button type="btn">age</button>
+      </form>
+      {age ? <p>{age}</p> : null}
+    </div>
+  );
+}
